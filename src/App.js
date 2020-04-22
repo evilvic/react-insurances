bd91148cc407a94b43b0abdc374da43ec8a5356c
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-22 13:51:19 -0500

    add form styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Header from './components/Header'
+import Form from './components/Form'
+
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const FormContainer = styled.div`
+  background: #fff;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header
-      title='Insurance Quotes'
-    />
+    <Container>
+      <Header
+        title='Insurance Quotes'
+        />
+        <FormContainer>
+          <Form />
+        </FormContainer>
+    </Container>
   )
 }
 
